e6a8125cba98e2f73800a7d49285e02152c31308
essivekkeli <96295581+essivekkeli@users.noreply.github.com>
2024-06-12 16:39:43 +0300

    Add files via upload

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const Parser = require('rss-parser');
+const cors = require('cors');
+const app = express();
+const parser = new Parser();
+
+app.use(cors());
+
+app.get('/rss', async (req, res) => {
+    let feed = await parser.parseURL('https://search.cnbc.com/rs/search/combinedcms/view.xml?partnerId=wrss01&id=100727362');
+    res.json(feed.items);
+});
+
+app.listen(3000, () => {
+    console.log('Serveri pystyssä portissa 3000');
+});
